4baff70d90af1cafd453fa37547ea61600c9cdcd
Sudeep Deysarker <47326452+Lunaticsatoshi@users.noreply.github.com>
2020-02-03 19:01:57 +0530

    Add files via upload

---
+++ New folder (2)/js/js1.js
@@ -0,0 +1,30 @@
+
+// Get the modal signup
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+//login seeker
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+var modal = document.getElementById('id03');
+//login company
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
